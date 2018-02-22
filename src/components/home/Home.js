79cb6e2ffaf0f2bf253c999619d7868d9ad67d9f
ridooo <ridooprent@gmail.com>
2018-02-22 16:59:43 +0700

    add: whats-new page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar';
 import Header from './Header';
+import WhatsNew from './WhatsNew';
 
 class Home extends Component {
   render() {
@@ -8,6 +9,7 @@ class Home extends Component {
         <div>
             <Navbar/>
             <Header/>
+            <WhatsNew/>
         </div>
     );
   }
---
+++ src/components/home/WhatsNew.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../../assets/WhatsNew.css';
+
+class WhatsNew extends Component {
+    render() {
+        return (
+            <div className="whats-new">
+                
+            </div>
+        );
+    }
+}
+
+export default WhatsNew;
