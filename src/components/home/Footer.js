a273524fa4c64be34f3163b42e7b203ae700e57f
ridoansaleh <ridoansalehnasution98@gmail.com>
2018-02-24 01:47:02 +0700

    add: footer menu

--- src/components/home/Footer.js
+++ src/components/home/Footer.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import fontawesome from '@fortawesome/fontawesome';
 import fabSpotify from '@fortawesome/fontawesome-free-brands/faSpotify';
+import fabInstagram from '@fortawesome/fontawesome-free-brands/faInstagram';
+import fabTwitter from '@fortawesome/fontawesome-free-brands/faTwitter';
+import fabFacebook from '@fortawesome/fontawesome-free-brands/faFacebook';
 import '../../assets/Footer.css';
 
 fontawesome.library.add(fabSpotify)
+fontawesome.library.add(fabInstagram)
+fontawesome.library.add(fabTwitter)
+fontawesome.library.add(fabFacebook)
 
 class Footer extends Component {
     render() {
@@ -16,18 +22,34 @@ class Footer extends Component {
                         </div>
                         <div className="perusahaan">
                             <h3>Perusahaan</h3>
+                            <ul>
+                                <li>Tentang</li>
+                                <li>Pekerjaan</li>
+                                <li>Pers</li>
+                                <li>Berita</li>
+                            </ul>
                         </div>
                         <div className="komunitas">
                             <h3>Komunitas</h3>
+                            <ul>
+                                <li>Untuk Artis</li>
+                                <li>Pengembang</li>
+                                <li>Brands</li>
+                            </ul>
                         </div>
                         <div className="tautan-berguna">
                             <h3>Tautan Berguna</h3>
+                            <ul>
+                                <li>Bantuan</li>
+                                <li>Hadiah</li>
+                                <li>Pemutar Web</li>
+                            </ul>
                         </div>
                     </div>
                     <div className="right-menu">
-                        <div className="instagram"></div>
-                        <div className="twitter"></div>
-                        <div className="facebook"></div>
+                        <i className="instagram fab fa-instagram"></i>
+                        <i className="twitter fab fa-twitter"></i>
+                        <i className="facebook fab fa-facebook"></i>
                     </div>
                 </div>
                 <div className="bottom">
